b9ede05824477307fcea2fb30da4072884aef701
Gideon Aina <gideonaina@live.com>
2024-12-11 15:09:06 -0500

    add iac to gitignore and changed theme color

--- iac/terraform/knowledge_base/modules/iam/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "s3_bucket_arn" {
-  description = "The ARN of the S3 bucket"
-  type        = string
-}
-
-variable "s3_bucket_name" {
-  description = "Knowledge Base S3"
-  type        = string
-}
--- iac/terraform/knowledge_base/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "endpoint_url" {
-  value = "https://runtime.sagemaker.${var.region}.amazonaws.com/endpoints/${module.sagemaker.endpoint_name}/invocations"
-}
